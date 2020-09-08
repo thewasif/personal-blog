111005b64a0a6f7007dfc98f7d7a51e59e6c88ae
MuhammadWasif <wasif33@outlook.com>
2020-09-08 15:18:08 +0500

    🔧 fix share button and routing issue

--- src/screens/post.jsx
+++ src/screens/post.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import ReactHtmlParser from "react-html-parser";
-import Layout from "../components/Layout";
-import ProfileCard from "../components/ProfileCard";
-import PageProgress from "react-page-progress";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
+import Layout from '../components/Layout';
+import ProfileCard from '../components/ProfileCard';
+import PageProgress from 'react-page-progress';
 
 let tweetHref = `https://twitter.com/intent/tweet?text=Check out this article by @m_wasif_&url=${window.location.href}`;
 let fbHref = `http://www.facebook.com/sharer.php?u=${window.location.href}`;
@@ -14,8 +14,8 @@ function Post(props) {
   let { slug } = useParams();
 
   useEffect(() => {
-    fetch(`https://dev.to/api/articles/${slug}`).then(e => {
-      e.json().then(res => {
+    fetch(`https://dev.to/api/articles/${slug}`).then((e) => {
+      e.json().then((res) => {
         let fetchedBlog = res;
         setBlog(fetchedBlog);
       });
@@ -23,50 +23,50 @@ function Post(props) {
   }, [slug]);
   return (
     <Layout>
-      <PageProgress color={"#333"} />
-      <div className="post-container">
-        <div className="likes-container">
+      <PageProgress color={'#333'} />
+      <div className='post-container'>
+        <div className='likes-container'>
           <div
             style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center"
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <i
-              className="fa fa-heart"
-              style={{ color: "#ff4444", textAlign: "center", fontSize: 20 }}
+              className='fa fa-heart'
+              style={{ color: '#ff4444', textAlign: 'center', fontSize: 20 }}
             ></i>
-            <p style={{ textAlign: "center", marginLeft: 4, fontSize: 20 }}>
+            <p style={{ textAlign: 'center', marginLeft: 4, fontSize: 20 }}>
               {blog.positive_reactions_count}
             </p>
           </div>
           <div
-            className="share"
+            className='share'
             style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center"
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <div
               style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center"
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <a href={tweetHref}>
                 <i
-                  className="fab fa-twitter"
+                  className='fab fa-twitter'
                   style={{
-                    color: "#00acee",
-                    textAlign: "center",
+                    color: '#00acee',
+                    textAlign: 'center',
                     fontSize: 20,
-                    padding: 4
+                    padding: 4,
                   }}
                 ></i>
               </a>
@@ -74,19 +74,19 @@ function Post(props) {
 
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <a href={fbHref}>
                 <i
-                  className="fab fa-facebook"
+                  className='fab fa-facebook'
                   style={{
-                    color: "#1778F2",
-                    textAlign: "center",
+                    color: '#1778F2',
+                    textAlign: 'center',
                     fontSize: 20,
-                    padding: 4
+                    padding: 4,
                   }}
                 ></i>
               </a>
@@ -94,19 +94,19 @@ function Post(props) {
             </div>
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               <a href={linkedInHref}>
                 <i
-                  className="fab fa-linkedin"
+                  className='fab fa-linkedin'
                   style={{
-                    color: "#006192",
-                    textAlign: "center",
+                    color: '#006192',
+                    textAlign: 'center',
                     fontSize: 20,
-                    padding: 4
+                    padding: 4,
                   }}
                 ></i>
               </a>
@@ -114,12 +114,12 @@ function Post(props) {
             </div>
           </div>
         </div>
-        <div className="post">
-          <img src={blog.cover_image} alt="cover" />
+        <div className='post'>
+          <img src={blog.cover_image} alt='cover' />
           <h1>{blog.title}</h1>
-          <div className="article">{ReactHtmlParser(blog.body_html)}</div>
+          <div className='article'>{ReactHtmlParser(blog.body_html)}</div>
         </div>
-        <div className="card-container">
+        <div className='card-container'>
           <ProfileCard />
         </div>
       </div>
